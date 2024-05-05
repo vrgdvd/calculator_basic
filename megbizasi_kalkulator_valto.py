0842ccfdcea6cc2cf028c0fe045640742016e14a
unknown <vargadavid90@protonmail.com>
2024-05-05 20:58:49 +0200

    kommit

---
+++ megbizasi_kalkulator_valto.py
@@ -0,0 +1,29 @@
+#Ingatlanos megbízási-kalkulátor
+"""
+m2=float(input("Alepterület m²-ben megadva: "))
+ar=float(input("Hirdetési ár: "))
+jut=float(input("Jutalék százalékban megadva: "))
+m2ar=float(input("Felbecsült piaci ár m²-ben megadva:"))
+
+juti=ar/100*jut
+afa=juti/100*27
+
+print ("Hirdtési ár alapján kalkulált m²-ár:", ar//m2,"-Ft/m².")
+
+print ("Jutalék nettó:",juti,"Ft")
+
+print ("Áfa(27%):",afa,"Ft")
+
+print ("Jutalék bruttó:",juti+afa,"Ft")
+
+print ("Ingatlan értéke a becsült m² ár alapján:",m2ar*m2,"Ft")
+"""
+
+#Euro FT váltó
+"""
+arfolyam=(input ("Kérem adja meg az aktuális napi árfolyamot:"))
+arfolyam=int (arfolyam)
+mennyiseg=(input ("Kérem adja meg, mennyi ft-ot szeretne átváltani:"))
+mennyiseg=int(mennyiseg)
+print (mennyiseg,'-Ft \n Összesen:',arfolyam*mennyiseg,'€')
+"""
